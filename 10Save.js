4cb647b2084d8b9123f23ec9b0944f6813ae42a0
Delvin <90351386+delvinsalman@users.noreply.github.com>
2024-12-08 09:59:42 -0500

    Delete 10Save.js

--- 10Save.js
+++
@@ -1,7 +0,0 @@
-function keyPressed() {
-  // Check if the key pressed is 's'
-  if (key === 's' || key === 'S') {
-    // Save the canvas as an image with a timestamp
-    saveCanvas('MyVisualArtPiece', 'png');
-  }
-}
