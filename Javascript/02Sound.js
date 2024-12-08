178e5cf1fb3c87d9a540915aa3fad96a29645cc3
Delvin <90351386+delvinsalman@users.noreply.github.com>
2024-12-08 10:00:50 -0500

    Add files via upload

---
+++ Javascript/02Sound.js
@@ -0,0 +1,19 @@
+function preload() {
+  // Sound for mouse click
+  clickSound = loadSound('Sound/S2-Draw.mp3');
+
+  // Sound for boid interactions
+  boidSound = loadSound('Sound/S5-Sparkle.mp3');
+
+  // Sound for clearing the canvas
+  clearSound = loadSound('Sound/S1-Clear.mp3');
+
+  // Background sound
+  backgroundSound = loadSound('Sound/S4-Song.mp3');
+
+  // Sound for background color change
+  colorChangeSound = loadSound('Sound/S2-Draw.mp3');
+  
+  // Sound for changing background color
+  colorChangeSound = loadSound('Sound/S3-Skip.mp3');
+}
